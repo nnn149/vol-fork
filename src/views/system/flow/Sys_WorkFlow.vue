4c7aa566090c4ae3d630cfd22aefc32bbab8a344
283591387@qq.com <jxx>
2022-08-15 04:03:46 +0800

    增加审批流程功能,更新代码：VOL.Core类库全部更新替换覆盖，UserInfo.cs、数据库更新，更新db文件夹中表结构Sys_WorkFlow、Sys_WorkFlowStep、Sys_WorkFlowTable、Sys_WorkFlowTableStep这4张图，同时生成代码(代码生成时类库选择system，文件夹名flow)

--- src/views/system/flow/Sys_WorkFlow.vue
+++ src/views/system/flow/Sys_WorkFlow.vue
@@ -24,48 +24,48 @@
             const table = ref({
                 key: 'WorkFlow_Id',
                 footer: "Foots",
-                cnName: '审批流程',
+                cnName: '审批流程配置',
                 name: 'flow/Sys_WorkFlow',
                 url: "/Sys_WorkFlow/",
                 sortName: "CreateDate"
             });
             const editFormFields = ref({"WorkName":"","WorkTable":"","Remark":""});
-            const editFormOptions = ref([[{"title":"流程名称","field":"WorkName"},
-                               {"title":"表名","field":"WorkTable"},
+            const editFormOptions = ref([[{"title":"流程名称","required":true,"field":"WorkName"},
+                               {"title":"表名","required":true,"field":"WorkTable"},
                                {"title":"备注","field":"Remark"}]]);
-            const searchFormFields = ref({"WorkName":"","WorkTable":"","Creator":"","ModifyDate":""});
-            const searchFormOptions = ref([[{"title":"流程名称","field":"WorkName"},{"title":"表名","field":"WorkTable"},{"title":"创建人","field":"Creator","type":"datetime"},{"title":"修改时间","field":"ModifyDate","type":"datetime"}]]);
+            const searchFormFields = ref({"WorkName":"","WorkTable":"","Enable":"","Creator":""});
+            const searchFormOptions = ref([[{"title":"流程名称","field":"WorkName"},{"title":"表名","field":"WorkTable"},{"dataKey":"enable","data":[],"title":"是否启用","field":"Enable","type":"select"},{"title":"创建人","field":"Creator","type":"datetime"}]]);
             const columns = ref([{field:'WorkFlow_Id',title:'WorkFlow_Id',type:'guid',width:110,hidden:true,readonly:true,require:true,align:'left'},
-                       {field:'WorkName',title:'流程名称',type:'string',link:true,width:140,align:'left',sort:true},
-                       {field:'WorkTable',title:'表名',type:'string',width:100,align:'left'},
+                       {field:'WorkName',title:'流程名称',type:'string',link:true,width:140,require:true,align:'left',sort:true},
+                       {field:'WorkTable',title:'表名',type:'string',width:100,require:true,align:'left'},
                        {field:'WorkTableName',title:'功能菜单',type:'string',width:120,align:'left'},
+                       {field:'Enable',title:'是否启用',type:'byte',bind:{ key:'enable',data:[]},width:100,align:'left'},
                        {field:'NodeConfig',title:'节点信息',type:'string',width:110,hidden:true,align:'left'},
                        {field:'LineConfig',title:'连接配置',type:'string',width:110,hidden:true,align:'left'},
                        {field:'Remark',title:'备注',type:'string',width:130,align:'left'},
                        {field:'CreateID',title:'CreateID',type:'int',width:80,hidden:true,align:'left'},
                        {field:'Creator',title:'创建人',type:'string',width:100,align:'left'},
                        {field:'CreateDate',title:'创建时间',type:'datetime',width:150,align:'left',sort:true},
-                       {field:'Enable',title:'Enable',type:'byte',width:110,hidden:true,align:'left'},
                        {field:'ModifyID',title:'ModifyID',type:'int',width:80,hidden:true,align:'left'},
                        {field:'Modifier',title:'修改人',type:'string',width:100,align:'left'},
                        {field:'ModifyDate',title:'修改时间',type:'datetime',width:160,align:'left',sort:true}]);
             const detail = ref({
-                cnName: "审批流程步骤",
+                cnName: "审批节点配置",
                 table: "Sys_WorkFlowStep",
                 columns: [{field:'WorkStepFlow_Id',title:'WorkStepFlow_Id',type:'guid',width:110,hidden:true,readonly:true,require:true,align:'left'},
                        {field:'WorkFlow_Id',title:'流程主表id',type:'guid',width:110,align:'left',sort:true},
-                       {field:'Name',title:'节点名称',type:'string',width:180,edit:{type:''},align:'left'},
-                       {field:'Step',title:'顺序步骤',type:'int',width:110,edit:{type:''},align:'left'},
-                       {field:'NodeType',title:'节点类型(1=按用户审批,2=按角色审批)',type:'int',width:110,edit:{type:''},align:'left'},
-                       {field:'UserId',title:'审批用户id',type:'int',width:110,edit:{type:''},align:'left'},
-                       {field:'RoleId',title:'审批角色id',type:'int',width:110,edit:{type:''},align:'left'},
-                       {field:'Remark',title:'Remark',type:'string',width:220,align:'left'},
-                       {field:'CreateDate',title:'CreateDate',type:'datetime',width:110,align:'left',sort:true},
+                       {field:'StepId',title:'流程节点Id',type:'string',width:120,align:'left'},
+                       {field:'StepName',title:'节点名称',type:'string',width:110,align:'left'},
+                       {field:'StepType',title:'节点类型(1=按用户审批,2=按角色审批)',type:'int',width:110,align:'left'},
+                       {field:'StepValue',title:'审批用户id或角色id',type:'int',width:110,align:'left'},
+                       {field:'Remark',title:'备注',type:'string',width:220,align:'left'},
+                       {field:'OrderId',title:'审批顺序',type:'int',width:80,align:'left'},
+                       {field:'CreateDate',title:'创建时间',type:'datetime',width:110,align:'left',sort:true},
                        {field:'CreateID',title:'CreateID',type:'int',width:80,hidden:true,align:'left'},
-                       {field:'Creator',title:'Creator',type:'string',width:130,align:'left'},
+                       {field:'Creator',title:'创建人',type:'string',width:130,align:'left'},
                        {field:'Enable',title:'Enable',type:'byte',width:110,align:'left'},
-                       {field:'Modifier',title:'Modifier',type:'string',width:130,align:'left'},
-                       {field:'ModifyDate',title:'ModifyDate',type:'datetime',width:110,align:'left',sort:true},
+                       {field:'Modifier',title:'修改人',type:'string',width:130,align:'left'},
+                       {field:'ModifyDate',title:'修改时间',type:'datetime',width:110,align:'left',sort:true},
                        {field:'ModifyID',title:'ModifyID',type:'int',width:80,hidden:true,align:'left'}],
                 sortName: "CreateDate",
                 key: "WorkStepFlow_Id"
